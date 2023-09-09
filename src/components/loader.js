e9dc959b8c6ce8ec6a9452a7d9f076523ea33c01
Sarim Hassan <sarimhassan5@gmail.com>
2023-09-09 15:46:53 +0500

    updated the app header and shop icon and add new color for chip

--- src/components/loader.js
+++ src/components/loader.js
@@ -3,10 +3,22 @@ import Lottie from 'lottie-react'
 import { LoaderAnimation } from '../assets/animations'
 
 const Loader = ({ visible }) => {
-    if(!visible) return null
+  if (!visible) return null
   return (
-      <div style={{ width: '100%', height: '100%', display: 'flex', justifyContent:'center',alignItems:'center',position:'fixed',opacity:'75%',background:'white',zIndex:9999}}>
-      <Lottie animationData={LoaderAnimation} loop={true} />
+    <div
+      style={{
+        width: '100%',
+        height: '100%',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        position: 'fixed',
+        opacity: '75%',
+        background: 'white',
+        zIndex: 9999
+      }}
+    >
+      <Lottie animationData={LoaderAnimation} width={50} height={50} loop={true} />
     </div>
   )
 }
