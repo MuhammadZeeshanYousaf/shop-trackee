1030ebb2cd1ce768a36944bda69b6746edd8adaa
Sarim Hassan <sarimhassan5@gmail.com>
2023-09-21 23:27:59 +0500

    updated the dashboard

--- src/components/WelcomeCard.js
+++ src/components/WelcomeCard.js
@@ -4,6 +4,7 @@ import Button from '@mui/material/Button'
 import { styled } from '@mui/material/styles'
 import Typography from '@mui/material/Typography'
 import CardContent from '@mui/material/CardContent'
+import { useRouter } from 'next/router'
 
 const Illustration = styled('img')(({ theme }) => ({
   right: 20,
@@ -16,17 +17,20 @@ const Illustration = styled('img')(({ theme }) => ({
 }))
 
 const WelcomeCard = () => {
+  const user = JSON.parse(localStorage.getItem('userData'))
+  const route = useRouter()
+
   return (
     <Card sx={{ position: 'relative' }}>
       <CardContent>
         <Typography variant='h5' sx={{ mb: 0.5 }}>
-          Welcome John 
+          Welcome {user?.name}
         </Typography>
         <Typography sx={{ mb: 2, color: 'text.secondary' }}>to Shop Trackee</Typography>
-        <Typography variant='h4' sx={{ mb: 0.75, color: 'primary.main' }}>
-          
-        </Typography>
-        <Button sx={{mt:3}} variant='contained'>View Shops</Button>
+        <Typography variant='h4' sx={{ mb: 0.75, color: 'primary.main' }}></Typography>
+        <Button sx={{ mt: 3 }} variant='contained' onClick={()=>route.push('/shop')}>
+          View Shops
+        </Button>
         <Illustration width={116} alt='congratulations john' src='/images/congratulations-john.png' />
       </CardContent>
     </Card>
