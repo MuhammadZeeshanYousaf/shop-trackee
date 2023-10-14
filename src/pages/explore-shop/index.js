b21a59041a919284509392203f277d12692913e4
Sarim Hassan <sarimhassan5@gmail.com>
2023-10-14 18:30:29 +0500

    added pagination on the shop products and services page

--- src/pages/explore-shop/index.js
+++ src/pages/explore-shop/index.js
@@ -3,7 +3,7 @@ import { useRouter } from 'next/router'
 import { useLoader } from 'src/hooks'
 import { Network, Url } from 'src/configs'
 import { showErrorMessage, showSuccessMessage, CustomerProductCard } from 'src/components'
-import { Typography, Grid, Box } from '@mui/material'
+import { Typography, Grid, Box, Pagination } from '@mui/material'
 import ServiceCard from '../shop/products-and-services/ServiceCard'
 
 import Icon from 'src/@core/components/icon'
@@ -13,6 +13,14 @@ const ExploreShop = () => {
   const { setLoader } = useLoader()
   const [products, setProducts] = useState([])
   const [services, setServices] = useState([])
+
+  const [totalProductPages, setTotalProductPages] = useState(1)
+
+  const [totalServicesPage, setTotalServicesPage] = useState(1)
+
+  const [currentProductPage, setCurrentProductPage] = useState(1)
+
+  const [currentServicePage, setCurrentServicePage] = useState(1)
 
   const NoResults = ({ value }) => {
     return (
@@ -40,12 +48,14 @@ const ExploreShop = () => {
 
   const searchByShop = async () => {
     setLoader(true)
-    const response = await Network.get(Url.searchByShop(query?.shopId, 1, 1))
+    const response = await Network.get(Url.searchByShop(query?.shopId, currentProductPage, currentServicePage))
     setLoader(false)
     if (!response.ok) return showErrorMessage(response.data.message)
 
     setProducts(response.data.product.data)
+    setTotalProductPages(response.data.product.total_pages)
     setServices(response.data.service.data)
+    setTotalServicesPage(response.data.service.total_pages)
   }
 
   const addToFavourite = async (id, status, type) => {
@@ -62,9 +72,17 @@ const ExploreShop = () => {
     searchByShop()
   }
 
+  const handleProductPage = (event, value) => {
+    setCurrentProductPage(value)
+  }
+
+  const handleServicePage = (event, value) => {
+    setCurrentServicePage(value)
+  }
+
   useEffect(() => {
     searchByShop()
-  }, [])
+  }, [currentProductPage, currentServicePage])
 
   return (
     <div>
@@ -87,6 +105,11 @@ const ExploreShop = () => {
           <NoResults value={'Products'} />
         )}
       </Grid>
+
+      <div style={{ display: 'flex', justifyContent: 'center', marginTop: '10px' }}>
+        <Pagination count={totalProductPages} onChange={handleProductPage} />
+      </div>
+
       <Typography sx={{ mt: 5 }} variant='h2'>
         Services
       </Typography>
@@ -108,6 +131,10 @@ const ExploreShop = () => {
           <NoResults value={'Services'} />
         )}
       </Grid>
+
+      <div style={{ display: 'flex', justifyContent: 'center', marginTop: '10px' }}>
+        <Pagination count={totalServicesPage} onChange={handleServicePage} />
+      </div>
     </div>
   )
 }
